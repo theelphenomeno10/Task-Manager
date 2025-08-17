c238be82653aa28b21587953a233ef250b48f1ad
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-08-17 22:30:10 +0700

    feat: app configuration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const tasks = require('./route/tasks.js')
+const errorHandler = require('./middlewares/error_handling.js')
 
 app.use(express.json())
 
-
+app.use('/api/v1/tasks', tasks)
+app.use(errorHandler);
 
 module.exports = app
