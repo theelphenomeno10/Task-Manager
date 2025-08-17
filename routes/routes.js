0c9ce46d5400a836ed91f7efb0d2495693583cba
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-08-17 22:18:50 +0700

    feat: routes added

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const {
+    getAllTask, 
+    createTask, 
+    getTask, 
+    updateTask, 
+    deleteTask
+} = require('../controller/tasks.js')
+
+router.route('/').get(getAllTask).post(createTask)
+router.route('/:id').get(getTask).delete(deleteTask).patch(updateTask)
+
+module.exports = router
