e90856bad0809abbbb7c392e41d7e4d02604c427
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-08-20 18:47:14 +0700

    feat: authenticate middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const generateToken = require('../utils/generate_token.js')
+
+const authenticateJWT = (req, res, next) => {
+    const authHead = req.headers.authorization
+
+    if (!authHead || !authHead.startsWith("Bearer ")) {
+        return res.status(401).json({msg: 'No token provided'})
+    }
+
+    const token = authHead.split(" ")[1]
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(403).json({msg: 'Invalid or expired token'})
+    }
+}
+
+module.exports = authenticateJWT
