956604dd05c0fde3b3cea3747d704b040bef1c5b
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-08-20 23:51:21 +0700

    feat: register and login

--- controller/authentication.js
+++ controller/authentication.js
@@ -1,5 +1,33 @@
 const generateToken = require('../utils/generate_token')
+const User = require('../models/users.js')
+const asyncWrapper = require('../middlewares/async.js')
 
-const register = async (req, res) => {
-    
-}
+const register = asyncWrapper (async (req, res) => {
+    const {username, password, email, role}  = req.body
+    const user = await User.create({username, email, password, role})
+    const token = generateToken(user)
+    res.status(201).json({
+        msg: 'User registered successfully',
+        user: {username, email},
+        token: token
+    })
+})
+
+const login = asyncWrapper (async (req, res) => {
+    const {username, password} = req.body
+    const user = await User.findOne({username})
+
+    if (!user) {
+        return res.status(401).json({msg: 'Invalid username'})
+    }
+
+    if (!(await user.comparePassword(password))) {
+        return res.status(401).json({msg: 'Wrong password'})
+    }
+
+    const token = generateToken(user)
+
+    res.status(200).json({msg: 'Login successful', token: token})
+})
+
+module.exports = {register, login}
