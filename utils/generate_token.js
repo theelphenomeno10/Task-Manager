5ce7834f7c46b056f0de8227eee216aab682a1fc
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-08-20 17:52:27 +0700

    add: jwt token generator

---
+++ utils/generate_token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (user) => {
+    return jwt.sign(
+        {
+            id: user._id,
+            username: user.username,
+            role: user.role
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: "1h"
+        }
+    )
+}
+
+module.exports = generateToken
